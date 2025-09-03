be43d6e629ae1ecec85b5a33b68abf2cc9ff1516
v2f <filipevictor94@gmail.com>
2025-09-02 21:56:34 -0300

    meu primeiro desafio da plataforma 3000 Maximum Area of Longest Diagonal Rectangle

---
+++ problems/problems.py
@@ -0,0 +1,35 @@
+
+
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: list[list[int]]) -> int:
+        """
+        3000 Maximum Area of Longest Diagonal Rectangle
+
+        Exemplo 1
+        >>> dimensions = [[9,3],[8,6]]
+        >>> 48
+
+        Exemplo 2
+        >>> dimensions = [[3,4],[4,3]]
+        >>> 12
+        """
+        areas: list = []
+        hipotenusas: list = []
+
+        for index, _ in enumerate(dimensions):
+            catetoI, catetoII, *_ = dimensions[index]
+
+            area: int = catetoI * catetoII
+            hipotenusa: float = (catetoI**2 + catetoII**2)**0.5 
+
+            areas.append(area)
+            hipotenusas.append(hipotenusa)
+
+        maior_hipotenusa = max(hipotenusas)
+        contador_maior_hipotenusa = hipotenusas.count(maior_hipotenusa)
+
+        if contador_maior_hipotenusa > 1:
+            areas_filtradas: list = [areas[i] for i, d in enumerate(hipotenusas) if d == maior_hipotenusa]
+            return max(areas_filtradas)            
+        else:
+            return areas[hipotenusas.index(maior_hipotenusa)]
